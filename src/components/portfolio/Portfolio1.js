0e808d49598f9a592731b7d5be55a4bf625ebeaf
ramprits <ramprits-sdei@gmail.com>
2020-12-04 09:59:32 +0530

    adding netlify

--- src/components/portfolio/Portfolio1.js
+++ src/components/portfolio/Portfolio1.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
 
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardHeader from "@material-ui/core/CardHeader";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   media: {
     height: "256px",
   },
